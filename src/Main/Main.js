8a8b03c037166c82401e67584f68f93b7b3ac0f1
CheNikos <chenikos@yandex.ru>
2023-01-09 23:44:21 +0300

    начал понимать что к чему

---
+++ src/Main/Main.js
@@ -0,0 +1,20 @@
+function Main() {
+    return (
+        <main>
+            <section className="profile">
+                <div className="profile__info">
+                    <div>
+                        <h2 className="profile__title"></h2>
+                        <h2 className="profile__job"></h2>
+                        <button className="profile__button"></button>
+                    </div>
+                </div>
+            </section>
+            <section className="card-container>">
+
+            </section>
+        </main>
+    )
+}
+
+export default Main;
