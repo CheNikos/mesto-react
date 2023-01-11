814a3ab8c0dccc71e14bda5caa558b838d14e48f
CheNikos <chenikos@yandex.ru>
2023-01-11 22:10:19 +0300

    Не могу понять от куда и почему ошибки

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -2,15 +2,15 @@ export default function ImagePopup(props) {
   return (
     <section className={`${props.card ? `popup popup_opened` : `popup`}`}>
       <div className="popup__container popup__container_zoom">
+      <button
+          className="popup__close popup__close_zoom"
+          type="button"
+          onClick={props.onClose}
+        />
         <img
           className="popup__zoom-image"
           src={`${props.linkCard.link}`}
           alt={props.linkCard.name}
-        />
-        <button
-          className="popup__close popup__close_zoom"
-          type="button"
-          onClick={props.onClose}
         />
         <h2 className="popup__name-zoom">{props.linkCard.name}</h2>
       </div>
